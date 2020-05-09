70e236a8144317bd831ee0312aaab30c285ba1b3
Raul Silva Sousa <rraulsilva.dev@gmail.com>
2020-05-09 08:03:19 -0300

    Adding the migration training

---
+++ src/database/migrations/20200509104139-create-trainings.js
@@ -0,0 +1,50 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+      Example:
+      */
+    return queryInterface.createTable("trainings", {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          key: "id",
+          model: "users",
+        },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+      Example:
+      */
+    return queryInterface.dropTable("trainings");
+  },
+};
